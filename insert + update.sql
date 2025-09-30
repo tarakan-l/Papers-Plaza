2ff7ae53c88cb509643d4ce64b87731c12cbc6cf
Vinder1 <74203035+Vinder1@users.noreply.github.com>
2025-09-30 22:19:27 +0300

    Update and rename insert.sql to insert + update.sql

--- insert + update.sql
+++ insert + update.sql
@@ -27,3 +27,16 @@ INSERT INTO Items.LuggageItem(itemName, luggage_id) VALUES
 
 INSERT INTO Criminal.Case(crimeId, passportId) VALUES 
 (1, 1);
+
+
+UPDATE Items.LuggageItem
+SET itemName = 'Пистолет ПМ'
+WHERE id = 1;
+
+UPDATE Criminal.Record
+SET description = 'Кража драгоценностей из музея'
+WHERE id = 1;
+
+UPDATE Items.LuggageItem
+SET itemName = 'Лабубу'
+WHERE id = 2;
