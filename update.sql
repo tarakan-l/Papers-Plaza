59479fc6658b4cf26d69ff5159604d02f773878b
Vinder1 <74203035+Vinder1@users.noreply.github.com>
2025-09-30 22:17:14 +0300

    Create update.sql

---
+++ update.sql
@@ -0,0 +1,11 @@
+UPDATE Items.LuggageItem
+SET itemName = 'Пистолет ПМ'
+WHERE id = 1;
+
+UPDATE Criminal.Record
+SET description = 'Кража драгоценностей из музея'
+WHERE id = 1;
+
+UPDATE Items.LuggageItem
+SET itemName = 'Лабубу'
+WHERE id = 2;
