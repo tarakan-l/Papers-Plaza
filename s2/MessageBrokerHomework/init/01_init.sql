de4e533bc851ccb766fc8e161ec9dc9aa18992da
tarakan <enderok2@gmail.com>
2026-05-20 02:48:17 +0300

    ultra balbes code

---
+++ s2/MessageBrokerHomework/init/01_init.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE IF NOT EXISTS tasks (
+                                     id SERIAL PRIMARY KEY,
+                                     payload TEXT NOT NULL,
+                                     status VARCHAR(20) DEFAULT 'Ready',
+    priority INT DEFAULT 0,
+    attempts INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT now(),
+    scheduled_at TIMESTAMP DEFAULT now()
+    );
+
+CREATE INDEX IF NOT EXISTS idx_tasks_ready_process
+    ON tasks (priority DESC, scheduled_at ASC)
+    WHERE status = 'Ready';
